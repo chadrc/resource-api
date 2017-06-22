5aa91926f8812c8467e64f500b9c3fec27aa384c
chadrc <chadrcollins91@gmail.com>
2017-06-21 21:21:01 -0400

    Initial commit

---
+++ src/main/java/com/chadrc/resourceapi/ResourceapiApplication.java
@@ -0,0 +1,12 @@
+package com.chadrc.resourceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/chadrc/resourceapi/ResourceapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chadrc.resourceapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResourceapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
