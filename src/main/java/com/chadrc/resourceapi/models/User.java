0d95e2617ce1aba1655c43f1a569894898b81388
chadrc <chadrcollins91@gmail.com>
2017-06-23 06:34:15 -0400

    Added id to User model

--- src/main/java/com/chadrc/resourceapi/models/User.java
+++ src/main/java/com/chadrc/resourceapi/models/User.java
@@ -1,13 +1,22 @@
 package com.chadrc.resourceapi.models;
 
 import com.chadrc.resourceapi.annotations.ResourceModel;
+import org.springframework.data.annotation.Id;
 
 @ResourceModel
 public class User {
+
+    @Id
+    private String id;
+
     private String firstName;
 
     public User(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getFirstName() {
