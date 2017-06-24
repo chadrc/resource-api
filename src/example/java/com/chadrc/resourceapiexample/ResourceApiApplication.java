7520670c0bdee653609cf907a16f72f971d5a739
chadrc <chadrcollins91@gmail.com>
2017-06-24 16:02:22 -0400

    Corrected application name

--- src/example/java/com/chadrc/resourceapiexample/ResourceApiApplication.java
+++ src/example/java/com/chadrc/resourceapiexample/ResourceApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ResourceapiApplication {
+public class ResourceApiApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ResourceapiApplication.class, args);
+		SpringApplication.run(ResourceApiApplication.class, args);
 	}
 }
