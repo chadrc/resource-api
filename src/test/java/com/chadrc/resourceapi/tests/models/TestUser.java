a22e983d0c946087ae0b40dbc964f5276422537d
chadrc <chadrcollins91@gmail.com>
2017-06-27 23:33:12 -0400

    Setup tests for resource api

---
+++ src/test/java/com/chadrc/resourceapi/tests/models/TestUser.java
@@ -0,0 +1,8 @@
+package com.chadrc.resourceapi.tests.models;
+
+import com.chadrc.resourceapi.service.ResourceModel;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestUser implements ResourceModel {
+}
