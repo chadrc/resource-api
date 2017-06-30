147f7f3e40fb95e84b0158342704505c8d38bb3f
chadrc <chadrcollins91@gmail.com>
2017-06-29 21:04:21 -0400

    Made test for page number

--- src/test/java/com/chadrc/resourceapi/controller/ResourceControllerListTests.java
+++ src/test/java/com/chadrc/resourceapi/controller/ResourceControllerListTests.java
@@ -1,6 +1,7 @@
 package com.chadrc.resourceapi.controller;
 
 import com.chadrc.resourceapi.ResourceApiApplicationTests;
+import com.chadrc.resourceapi.domain.User;
 import com.chadrc.resourceapi.service.ResourcePage;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,26 +29,31 @@ public class ResourceControllerListTests {
         resourceControllerProxy.addUser("Charlie", "Gamma");
         resourceControllerProxy.addUser("Delta", "Delta");
         resourceControllerProxy.addUser("Echo", "Epsilon");
+
         resourceControllerProxy.addUser("Foxtrot", "Zeta");
         resourceControllerProxy.addUser("Golf", "Eta");
         resourceControllerProxy.addUser("Hotel", "Theta");
         resourceControllerProxy.addUser("India", "Iota");
         resourceControllerProxy.addUser("Juliet", "Kappa");
+
         resourceControllerProxy.addUser("Kilo", "Lambda");
         resourceControllerProxy.addUser("Lima", "Mu");
         resourceControllerProxy.addUser("Mike", "Nu");
         resourceControllerProxy.addUser("November", "Xi");
         resourceControllerProxy.addUser("Oscar", "Omicron");
+
         resourceControllerProxy.addUser("Papa", "Pi");
         resourceControllerProxy.addUser("Quebec", "Rho");
         resourceControllerProxy.addUser("Romeo", "Sigma");
         resourceControllerProxy.addUser("Sierra", "Tau");
         resourceControllerProxy.addUser("Tango", "Upsilon");
+
         resourceControllerProxy.addUser("Uniform", "Phi");
         resourceControllerProxy.addUser("Victor", "Chi");
         resourceControllerProxy.addUser("Whiskey", "Psi");
         resourceControllerProxy.addUser("X-Ray", "Omega");
         resourceControllerProxy.addUser("Yankee", "Yankee");
+
         resourceControllerProxy.addUser("Zulu", "Zulu");
     }
 
@@ -76,5 +82,18 @@ public class ResourceControllerListTests {
         ResourcePage resourcePage = (ResourcePage) responseEntity.getBody();
 
         assertEquals(resourcePage.getResources().size(), 5);
+    }
+
+    @Test
+    public void listUsersWithPage2HasSecondSetOfResources() {
+        PagingInfo pagingInfo = new PagingInfo();
+        pagingInfo.setPage(1);
+        ListOptions options = new ListOptions("User", pagingInfo);
+        ResponseEntity<Object> responseEntity = resourceControllerProxy.getResourceController().list(options);
+        ResourcePage resourcePage = (ResourcePage) responseEntity.getBody();
+
+        List resources = resourcePage.getResources();
+        User first = (User) resources.get(0);
+        assertEquals(first.getFirstName(), "Kilo");
     }
 }
