6dcd9dae651dd037a4dfafd3ab92baef536157cd
chadcollins <chadrcollins91@gmail.com>
2017-07-03 18:41:55 -0400

    Added core and coreTest source set, beginning api rewrite

---
+++ src/core/java/com/chadrc/resourceapi/ResourceController.java
@@ -0,0 +1,9 @@
+package com.chadrc.resourceapi;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResourceController {
+    private static Logger log = Logger.getLogger(ResourceController.class);
+}
