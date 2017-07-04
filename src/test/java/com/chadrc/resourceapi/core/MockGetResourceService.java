d240cfd7c3009dc5bb8409810f2f9a55352f2f34
chadcollins <chadrcollins91@gmail.com>
2017-07-03 22:48:41 -0400

    Renamed mock service to get to match test

--- src/test/java/com/chadrc/resourceapi/core/MockGetResourceService.java
+++ src/test/java/com/chadrc/resourceapi/core/MockGetResourceService.java
@@ -2,7 +2,7 @@ package com.chadrc.resourceapi.core;
 
 import org.springframework.http.ResponseEntity;
 
-public class MockResourceService implements ResourceService {
+public class MockGetResourceService implements ResourceService {
     @Override
     public ResponseEntity<GetResponse> fulfill(GetRequest request) {
         return ResponseEntity.ok(new GetResponse());
