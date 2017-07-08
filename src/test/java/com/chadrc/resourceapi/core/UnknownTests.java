d2deda216efd0447a03eaa9497cf6e385dbb11cb
chadrc <chadrcollins91@gmail.com>
2017-07-08 14:48:09 -0400

    Made tests for unknown methods

---
+++ src/test/java/com/chadrc/resourceapi/core/UnknownTests.java
@@ -0,0 +1,29 @@
+package com.chadrc.resourceapi.core;
+
+import com.chadrc.resourceapi.core.mocks.DeleteRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.head;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UnknownTestsConfig.class)
+public class UnknownTests extends BaseTests {
+
+    @Test
+    public void unsupportedMethodYields405() throws Exception {
+        mockMvc.perform(delete("/book")
+                .param("data", json(new DeleteRequest())))
+                .andExpect(status().isMethodNotAllowed());
+    }
+
+    @Test
+    public void headCallWithNoGetOrHeadServiceYields405() throws Exception {
+        mockMvc.perform(head("/book"))
+                .andExpect(status().isMethodNotAllowed());
+    }
+}
---
+++ src/test/java/com/chadrc/resourceapi/core/UnknownTestsConfig.java
@@ -0,0 +1,29 @@
+package com.chadrc.resourceapi.core;
+
+import com.chadrc.resourceapi.core.mocks.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ComponentScan(basePackages = "com.chadrc.resourceapi")
+@EnableWebMvc
+@WebAppConfiguration
+public class UnknownTestsConfig {
+    @Bean
+    public HttpMessageConverter getHttpMessageConverter() {
+        return new MappingJackson2HttpMessageConverter();
+    }
+
+    @Bean
+    public List<ResourceService> getResourceServices() {
+        return new ArrayList<ResourceService>() {{
+            add(new MockPostResourceService());
+        }};
+    }
+}
