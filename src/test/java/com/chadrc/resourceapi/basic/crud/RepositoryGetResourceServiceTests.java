a99241347f164169705f1b0d44e3c192e7ec5202
chadrc <chadrcollins91@gmail.com>
2017-07-09 22:49:03 -0400

    Added test data

--- src/test/java/com/chadrc/resourceapi/basic/crud/RepositoryGetResourceServiceTests.java
+++ src/test/java/com/chadrc/resourceapi/basic/crud/RepositoryGetResourceServiceTests.java
@@ -1,6 +1,8 @@
 package com.chadrc.resourceapi.basic.crud;
 
 import com.chadrc.resourceapi.BaseTests;
+import com.chadrc.resourceapi.models.Book;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,17 @@ public class RepositoryGetResourceServiceTests extends BaseTests {
 
     @Autowired
     private BookRepository bookRepository;
+
+    @Before
+    public void setup() {
+        bookRepository.delete(bookRepository.findAll());
+
+        bookRepository.insert(new Book("Book 1", "Test Author"));
+        bookRepository.insert(new Book("Book 2", "Test Author"));
+        bookRepository.insert(new Book("Book 3", "Test Author"));
+        bookRepository.insert(new Book("Book 4", "Fake Author"));
+        bookRepository.insert(new Book("Book 5", "Fake Author"));
+    }
 
     @Test
     public void contextLoads() {
