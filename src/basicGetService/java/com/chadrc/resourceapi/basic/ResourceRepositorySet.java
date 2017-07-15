a4d3bc25b25edac7891459a751424de9e3c3427a
chadrc <chadrcollins91@gmail.com>
2017-07-15 10:05:03 -0400

    ResourceRepositorySet now uses reflection utils for finding type args

--- src/basicGetService/java/com/chadrc/resourceapi/basic/ResourceRepositorySet.java
+++ src/basicGetService/java/com/chadrc/resourceapi/basic/ResourceRepositorySet.java
@@ -1,9 +1,9 @@
 package com.chadrc.resourceapi.basic;
 
+import com.chadrc.resourceapi.core.utils.ReflectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
@@ -16,20 +16,8 @@ public class ResourceRepositorySet {
     @Autowired
     public void setResourceRepositories(List<ResourceRepository> resourceRepositories) {
         for (ResourceRepository resourceRepository : resourceRepositories) {
-            Class repositoryClass = resourceRepository.getClass();
-            Type[] interfaces = ((Class)repositoryClass.getGenericInterfaces()[0]).getGenericInterfaces();
-            Type resourceRepositoryInterface = null;
-            for (Type inter : interfaces) {
-                if (inter instanceof ParameterizedType && ((ParameterizedType)inter).getRawType() == ResourceRepository.class) {
-                    resourceRepositoryInterface = inter;
-                }
-            }
-
-            if (resourceRepositoryInterface != null && resourceRepositoryInterface instanceof ParameterizedType) {
-                Type[] typeArgs = ((ParameterizedType) resourceRepositoryInterface).getActualTypeArguments();
-                Class resourceClass = (Class) typeArgs[0];
-                resourceRepositoriesByClass.put(resourceClass, resourceRepository);
-            }
+            Type[] typeArgs = ReflectionUtils.getTypeArgsForTypeFromObject(ResourceRepository.class, resourceRepository);
+            resourceRepositoriesByClass.put((Class) typeArgs[0], resourceRepository);
         }
     }
 
